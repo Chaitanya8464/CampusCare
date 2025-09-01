45d7f6efc9d8d7f81abaea253faa81b172e57755
Chaitanya Verma <chaitanya123@gmail.com>
2025-09-02 00:20:05 +0530

    integreated firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZmvqwyPhOXzwYhNkzphlrnz775MCtQFE",
+  authDomain: "campuscare-3a887.firebaseapp.com",
+  projectId: "campuscare-3a887",
+  storageBucket: "campuscare-3a887.firebasestorage.app",
+  messagingSenderId: "871251847754",
+  appId: "1:871251847754:web:6c2b87e77ea073330e14b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
